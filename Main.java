a5fd2eb7c38dfd1ef460566e9738a0ab920cbbdc
Bernarda <bernarda.schwalba@gmail.com>
2024-10-29 23:25:28 +0100

    zadatak

--- Main.java
+++ Main.java
@@ -1,41 +1,23 @@
-/*napraviti funkciju koja će provjeravati da li je lista uzlazno sortirana.
-prethodno, listu popunjava korisnik svojim unosima.
-funkcija treba vratiti true ili false.
-na kraju programa korisniku ispisati da li je lista sortirana ili nije.
- */
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+        public class Main {
+            public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        List<Integer> listaBrojeva = new ArrayList<>();
-        System.out.println("******** Unos 5 brojeva u listu ********** ");
+                Scanner scanner = new Scanner(System.in);
+                double[] apoeni = {500,200,100,50,20,10,5,2,1,
+                        0.5,0.2,0.1,0.05,0.02,0.01};
 
-        for (int i=0; i<5; i++){
-            System.out.println("Unesite " + (i+1) + ". broj u listu: ");
-            listaBrojeva.add(scanner.nextInt());
-        }
+                System.out.println("Unesite željeni iznos: ");
+                int iznos = scanner.nextInt();
+                int brojac = 0;
 
-        if (jeLiListaSortirana(listaBrojeva)){
-            System.out.print("Lista je uzlazno sortirana: ");
-            System.out.println(listaBrojeva);
-        }
-        else{
-            System.out.print("Lista nije uzlazno sortirana: ");
-            System.out.println(listaBrojeva);
-        }
-    }
-
-    private static Boolean jeLiListaSortirana(List<Integer> listaBrojeva){
-        for (int i=0; i< listaBrojeva.size()-1;i++){
-            if (listaBrojeva.get(i) > listaBrojeva.get(i + 1)){
-                return false;
+                for (double apoen : apoeni){
+                    double novcanica = iznos / apoen;
+                    if (novcanica >= 1) {
+                        System.out.println((int)novcanica + " x " + apoen);
+                        iznos -= (int)novcanica * apoen;
+                    }
+                }
             }
         }
-        return true;
-    }
-}
